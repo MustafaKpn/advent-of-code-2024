2f8e3102a1b1f47985b1d9f36efc2cda1a415a66
Mustafa Kaplan <mustafa.kaplan92@thg.com>
2024-12-02 17:38:33 +0000

    solved day1-part2

---
+++ day1/part2.py
@@ -0,0 +1,34 @@
+
+
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+left_list = []
+right_list = []
+
+for i in lines:
+    line_split = i.strip().split(' ')
+    if len(line_split) > 1:
+        left_list.append(int(line_split[0]))
+        right_list.append(int(line_split[3]))
+
+
+left_list_sorted = sorted(left_list)
+right_list_sorted = sorted(right_list)
+
+
+
+distances = []
+for i in range(len(left_list)):
+    distances.append(abs(left_list_sorted[i] - right_list_sorted[i]))
+
+
+
+similarity_scores = []
+
+for i in left_list:
+    similarity_scores.append(i * right_list.count(i))
+
+
+
