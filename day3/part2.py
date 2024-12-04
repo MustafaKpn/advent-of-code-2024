b2b9460f3c5d004faa4cb7355e3e39418479163f
Mustafa Kaplan <mustafa.kaplan92@thg.com>
2024-12-04 12:01:00 +0000

    solved day3-part2

--- day3/part2.py
+++ day3/part2.py
@@ -6,6 +6,7 @@ with open("input.txt", 'r') as f:
 all_does = re.sub(r"don't\(\).*?do", "do", read, flags=re.DOTALL)
 all_does = re.sub(r"don't\(\).*?do", "do", all_does, flags=re.DOTALL)
 all_does = re.sub(r"don't\(\).*?do", "do", all_does, flags=re.DOTALL)
+
 no_dont = re.sub(r"don't\(\).*", "", all_does, flags=re.DOTALL)
 
 muls = re.findall(r"mul\(\d{1,3},\d{1,3}\)", no_dont, flags=re.DOTALL)
